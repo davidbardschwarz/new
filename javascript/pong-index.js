799ac2a6540a869b44a86b04b156920ab8b60bbb
davidbardschwarz <63382529+davidbardschwarz@users.noreply.github.com>
2023-05-30 10:49:31 -0500

    Update pong-index.js

--- javascript/pong-index.js
+++ javascript/pong-index.js
@@ -260,12 +260,12 @@ function ballReset() {
 function ballCheckMax() {
   ball.direction.x = clamp(
     ball.direction.x,
-    -2 * ball.maximum.x,
+    -20 * ball.maximum.x,
     ball.maximum.x
   );
   ball.direction.y = clamp(
     ball.direction.y,
-    -2 * ball.maximum.y,
+    -20 * ball.maximum.y,
     ball.maximum.y
   );
 }
